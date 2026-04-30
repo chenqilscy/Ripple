34d83067fe652c319c5db6c724d8781d0718df8f
chenqi <chenqi_lscy@163.com>
2026-05-01 01:55:30 +0800

    feat(design-system): add unified ui component exports

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,17 @@
+export { default as Button } from './Button/Button'
+export type { ButtonProps } from './Button/Button'
+
+export {
+  ToastContainer as Toast,
+  useToast,
+  toast,
+  ToastProvider,
+} from './Toast/Toast'
+export type { ToastItem, ToastType } from './Toast/Toast'
+
+export { default as Tooltip } from './Tooltip/Tooltip'
+export { BRAND_TOOLTIPS } from './Tooltip/Tooltip'
+export type { TooltipProps } from './Tooltip/Tooltip'
+
+export { default as Modal } from './Modal/Modal'
+export type { ModalProps } from './Modal/Modal'
